958da07cfd585a4979255e50b85253f3959c6c80
Emmanuel Gyekye Atta-Penkra <emmanuel@jnsid.com>
2020-04-09 23:51:39 -0400

    Initial Commit

---
+++ Queen Green Smart Farm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Queen Green Smart Farm
+//
+//  Created by Emmanuel Gyekye Atta-Penkra on 4/9/20.
+//  Copyright © 2020 Special  Topics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
